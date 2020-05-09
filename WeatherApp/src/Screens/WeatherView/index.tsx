862d7bcd7920f59d961b9dfaaffa893a3618c156
sejin <sjsj0612@gmail.com>
2020-05-09 23:01:40 +0900

    Chapter 7

---
+++ WeatherApp/src/Screens/WeatherView/index.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, Alert } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+import Styled from 'styled-components/native';
+
+const Container = Styled.SafeAreaView`
+    flex: 1;
+    backgroud-color: #EEE;
+`;
+
+const WeatherContainer = Styled(FlatList)``;
+
+const LoadingView = Styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Loading = Styled.ActivityIndicator`
+    margin-bottom: 16px;
+`;
+
+const LoadingLabel = Styled.Text`
+    font-size: 16px;
+`;
+
+const WeatherItemContainer = Styled.View`
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Weather = Styled.Text`
+    margin-bottom: 16px;
+    font-size: 24px;
+    font-weight: bold;
+`;
+
+const Temperature = Styled.Text`
+    font-size: 16px;
+`;
+
+interface Props {}
+
+const API_KEY = '029170e6c4a0f910a84f2b519d3cad31';
+
+interface IWeather {
+    temperature?: number;
+    weather?: string;
+    isLoading: boolean;
+}
+
+const WeatherView = ({ }: Props) => {
+    const [weatherInfo, setWeatherInfo] = useState<IWeather>({
+        // 초기값 설정
+        temperature: undefined,
+        weather: undefined,
+        isLoading: false,
+    });
+
+    const getCurrenctWeather = () => {
+        setWeatherInfo({
+            isLoading: false,
+        });
+
+        Geolocation.getCurrentPosition(
+            position => {
+                const { latitude, longitude } = position.coords;
+                fetch(
+                    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+                )
+                .then(response => response.json())
+                .then(json => {
+                    setWeatherInfo({
+                        temperature: json.main.temp,
+                        weather: json.weather[0].mean,
+                        isLoading: true,
+                    });
+                })
+                .catch(error => {
+                    setWeatherInfo({
+                        isLoading: true,
+                    });
+                    showError('날씨 정보를 가져오는데 실패하였다.');
+                });
+            }
+        );
+    };
+
+    const showError = (message: string): void => {
+        setTimeout(() => {
+            Alert.alert(message);
+        }, 500);
+    };
+
+    useEffect(() => {
+        getCurrenctWeather();
+    }, []);
+
+    let data = [];
+    const { isLoading, weather, temperature } = weatherInfo;
+    if (weather && temperature) {
+        data.push(weatherInfo);
+    }
+
+    return (
+        <Container>
+            <WeatherContainer 
+                onRefresh={() => getCurrenctWeather}
+                refreshing={!isLoading}
+                data={data}
+                keyExtractor={(item, index) => {
+                    return `Weather-${index}`;
+                }}  
+                ListEmptyComponent={
+                    <LoadingView>
+                        <Loading size="large" color="#1976D2" />
+                        <LoadingLabel>Loading...</LoadingLabel>
+                    </LoadingView>
+                }
+                renderItem={({ item, index }) => (
+                    <WeatherItemContainer>
+                        <Weather>{(item as IWeather).weather}</Weather>
+                        <Temperature>({(item as IWeather).temperature}C)</Temperature>
+                    </WeatherItemContainer>
+                )}
+                contentContainerStyle={{ flex: 1 }}
+            />
+        </Container>
+    );
+};
+
+export default WeatherView;
